4c693bcb57e075417df39f034d2dc9600eceb65d
Byron Smith <byrondsmith9@gmail.com>
2024-12-31 17:39:57 -0500

    Added context and custom Hook folders, as well as one context and one hook. Created home and login pages that use these hooks and contexts.

--- advanced-e-commerce-react/src/App.jsx
+++ advanced-e-commerce-react/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Homepage from './components/HomePage';
+import UserContext from './context/UserContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const [user, setUser] = useState({ name: '', isLoggedIn: false }) // Used to update UserContext
+  // setUser in state overrides setUser in context
+ // Everything here has access to context
+  return (                        
+    <UserContext.Provider value={{ user, setUser }}>  {/* Values for functionality that was set up in UserContext */}
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/home' element={<Homepage/>} />
+        </Routes>
+      </Router>
+    </UserContext.Provider>
   )
 }
 
