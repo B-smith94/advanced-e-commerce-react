b1e111954e8f0906425be2af35296ca6ec3c77cb
b-smith94 <byrondsmith9@gmail.com>
2025-01-02 20:24:35 -0500

    added productsSlice, updated ProductCatalog to reflect asynchronous fetch from API

--- advanced-e-commerce-react/src/components/ProductCatalog.jsx
+++ advanced-e-commerce-react/src/components/ProductCatalog.jsx
@@ -1,10 +1,22 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { addItem } from '../features/cart/cartSlice';
-import products from '../data/product';
-import { Card, Button } from 'react-bootstrap';
+import { fetchProducts } from '../features/products/productsSlice';
+//import products from '../data/product';
+import { Card, Button, Row, Col, Spinner, Alert } from 'react-bootstrap';
 
 const ProductCatalog = () => {
     const dispatch = useDispatch(); // creates dispatch
+
+    const products = useSelector((state) => state.products.items);
+    const productsStatus = useSelector((state) => state.products.status);
+    const error = useSelector((state) => state.products.error);
+
+    useEffect(() => {
+        if (productsStatus === 'idle') {
+            dispatch(fetchProducts()); // gonna result in a promise
+        }
+    }, [productsStatus, dispatch]);
 
     const handleAddToCart = (id) => {
         dispatch(addItem({ id })); // pass id into addItem, which is passed into dispatch so that store can check for actions
@@ -12,20 +24,25 @@ const ProductCatalog = () => {
 
     return (
         <div>
-            <h2>Product Catalog</h2>
-            <div>
-                {products.map(product => (
-                    <Card key={product.id} style={{ width: '18rem', margin: '10px' }}>
+            <h2>Product Catalog</h2> {/* productsStatus matches addCases in builder in productsSlice */}
+            {productsStatus === 'loading' && <Spinner animation='border' role='status'><span className='visually-hidden'>Loading...</span></Spinner>}
+            {productsStatus === 'failed' && <Alert variant='danger'>{error}</Alert>}
+            <Row xs={1} md={4} className='g-4'>
+            {products.map(product => (
+                <Col key={product.id}>
+                    <Card style={{ width: '18rem' }}>
+                        <div style={{ padding: '10px' }}>
+                            <Card.Img variant='top' src={product.image} style={{ height: '250px', objectFit: 'contain' }} />
+                        </div>
                         <Card.Body>
-                            <Card.Title>{product.name}</Card.Title>
-                            <Card.Text>
-                                Price: ${product.price}    
-                            </Card.Text>    
+                            <Card.Title>{product.title}</Card.Title>
+                            <Card.Text>Price: ${product.price}</Card.Text>
                             <Button variant='primary' onClick={() => handleAddToCart(product.id)}>Add to Cart</Button>
-                        </Card.Body>   
+                        </Card.Body>
                     </Card>
-                ))}
-            </div>
+                </Col>
+            ))}
+            </Row>
         </div>
     );
 };
--- advanced-e-commerce-react/src/store.jsx
+++ advanced-e-commerce-react/src/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
+import productReducer from './features/products/productsSlice'
 
 export const store = configureStore({
     reducer: {
+        products: productReducer,
         cart: cartReducer, // cartSlice.reducer, handles state. Use dispatch(<action>) to call, cartReducer will find action that matches and perform that action
     },
 });
